33071baf4ec6d0ec83986f2a1e52a49073678eae
chengdongwu <chengdongwu52@gmail.com>
2021-05-12 10:11:57 +0800

    拉勾教育系统v1

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,9 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // 'semi': ['error', 'always']
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-explicit-any": ["off"],
-    "@typescript-eslint/explicit-module-boundary-types": ["off"],
-    "@typescript-eslint/no-unused-vars": ["off"]
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/explicit-module-boundary-types': ['off'],
+    '@typescript-eslint/no-unused-vars': ['off']
   }
 }
